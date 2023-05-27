6773fdd15b96791fbb71dfc246095e94c5ee4e6d
supchyan <mikhailsupov@yandex.ru>
2023-05-28 02:03:28 +0300

    rain filter moved own mod

--- Explode/Tools/Visuals.cs
+++ Explode/Tools/Visuals.cs
@@ -25,11 +25,6 @@ namespace iLikeExplode.Explode.Tools {
         private int scG;
         private int scB;
 
-        // rain intensity
-
-        private float Intensity;
-        public static float RainTransition;
-
         public static Color RainbowColor;
         public static Color MasterColor;
 
@@ -95,36 +90,6 @@ namespace iLikeExplode.Explode.Tools {
             }
 
             MasterColor = new Color(scR, scG, scB); // SETTING COLOR
-
-            // RAIN FILTER
-
-            if (!Filters.Scene["RainFilter"].IsActive()) 
-            Filters.Scene.Activate("RainFilter"); 
-
-            Tile tile = Main.tile[Main.LocalPlayer.Center.ToTileCoordinates()];
-            bool WallCollision = tile.WallType > WallID.None;
-
-            if(Main.raining && !WallCollision && 
-            !Main.LocalPlayer.ZoneSandstorm && !Main.LocalPlayer.ZoneSnow && 
-            !Main.LocalPlayer.ZoneNormalSpace && Main.LocalPlayer.position.Y < 5430f) 
-            {
-                RainTransition+=0.01f;
-                if(RainTransition > 1f)
-                RainTransition = 1f;
-                if(Main.rainTime <= 1000) {
-                    Main.maxRain = (int)MathHelper.Lerp(0, Main.maxRain, (float)Main.rainTime/1000);
-                    Main.maxRaining = MathHelper.Lerp(0, Main.maxRaining, (float)Main.rainTime/1000);
-                }
-            }
-            else {
-                RainTransition-=0.01f;
-                if(RainTransition < 0f)
-                RainTransition = 0f;
-            }
-
-            Intensity = (Main.maxRain * Main.maxRaining / (20.0f * 645.0f));
-
-            Filters.Scene["RainFilter"].GetShader().UseOpacity(Intensity*RainTransition);
 
         }
     }
--- iLikeExplode.cs
+++ iLikeExplode.cs
@@ -10,13 +10,7 @@ using Terraria.Graphics.Shaders;
 namespace iLikeExplode {
 	public class iLikeExplode : Mod {
 		public override void Load() {
-            if (Main.netMode != NetmodeID.Server) {
-
-                Ref<Effect> screenRef = new Ref<Effect>(ModContent.Request<Effect>("iLikeExplode/Explode/Tools/RainFilter", AssetRequestMode.ImmediateLoad).Value);
-				Filters.Scene["RainFilter"] = new Filter(new ScreenShaderData(screenRef, "RainFilter"), EffectPriority.VeryHigh);
-				Filters.Scene["RainFilter"].Load();
-            
-			}
+			
         }
 	}
 }
